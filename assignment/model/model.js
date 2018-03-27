dfbee71aae8939fac913aae1071ab20fa0add328
yiyangzhang <Chrismzyy@hotmail.com>
2018-03-27 11:34:38 -0700

    a5 done

--- assignment/model/model.js
+++ assignment/model/model.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 // this is haruko conncection
-var db = mongoose.connect('mongodb://chrismzyy@hotmail.com:135246Abcd@ds263847.mlab.com:63847/heroku_khn0t993');
+var db = mongoose.connect('mongodb://yiyangzhang:135246abcd@ds263847.mlab.com:63847/heroku_khn0t993');
 // var db = mongoose.connect('mongodb://localhost:27017/webdev', {useMongoClient: true});
 
 module.exports = db;
